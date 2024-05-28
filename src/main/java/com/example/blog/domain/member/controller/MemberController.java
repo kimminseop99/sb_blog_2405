7a823dd0ff99ff869ba8f440b1b427fb706bb395
kimminseop99 <minseopkim99@gmail.com>
2024-05-28 14:17:39 +0900

    회원가입 기능 추가

--- src/main/java/com/example/blog/domain/member/controller/MemberController.java
+++ src/main/java/com/example/blog/domain/member/controller/MemberController.java
@@ -1,6 +1,13 @@
 package com.example.blog.domain.member.controller;
 
+import com.example.blog.domain.member.service.MemberService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +18,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequiredArgsConstructor
 @RequestMapping("/member")
 public class MemberController {
+
+    private final MemberService memberService;
 
     @PreAuthorize("isAnonymous()")
     @GetMapping("/login")
@@ -23,5 +32,38 @@ public class MemberController {
         return "member/signup";
     }
 
+    @PostMapping("/signup")
+    public String signup(@Valid SignForm signForm){
+
+        memberService.signup(signForm.getUsername(), signForm.getPassword(), signForm.getNickname(), signForm.getEmail());
+
+        return "redirect:/member/login";
+    }
+
+
+    @ToString
+    @Getter
+    @Setter
+    public static class SignForm{
+        @NotBlank
+        @Length(min = 3)
+        private String username;
+
+        @NotBlank
+        @Length(min = 4)
+        private String password;
+
+        @NotBlank
+        @Length(min = 4)
+        private String password_confirm;
+
+        @NotBlank
+        @Length(min = 3)
+        private String nickname;
+
+        @NotBlank
+        @Length(min = 4)
+        private String email;
+    }
 
 }
